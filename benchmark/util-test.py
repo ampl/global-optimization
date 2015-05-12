7f40f138b5a3f1993a12a58421b01ec16d25cf53
vitaut <victor.zverovich@gmail.com>
2015-05-12 16:32:45 -0700

    Add read_solution

--- benchmark/util-test.py
+++ benchmark/util-test.py
@@ -1,9 +1,9 @@
 # The util module tests
 
 import os, tempfile, time, util
-from subprocess import check_call
+from subprocess import check_call, PIPE
 
-solver = './mock-solver'
+solver = 'couenne'
 
 def test_files():
   f = util.files('foo', '''
@@ -11,6 +11,12 @@ def test_files():
     baz
     ''')
   assert(f == ['foo/bar', 'foo/baz'])
+
+def test_sha1_file():
+  with tempfile.NamedTemporaryFile() as f:
+    f.write('some useful content')
+    f.flush()
+    assert(util.sha1_file(f.name) == '4f7a376f6110cb8aad4f02e319b52f7325d63a83')
 
 def test_temp_nl_file():
   with tempfile.NamedTemporaryFile() as ampl_file:
@@ -23,18 +29,22 @@ def test_temp_nl_file():
       assert(os.path.exists(nl_filename))
     assert(not os.path.exists(nl_filename))
 
-def test_sha1_file():
-  with tempfile.NamedTemporaryFile() as f:
-    f.write('some useful content')
-    f.flush()
-    assert(util.sha1_file(f.name) == '4f7a376f6110cb8aad4f02e319b52f7325d63a83')
+def test_read_solution():
+  with tempfile.NamedTemporaryFile() as ampl_file:
+    ampl_file.write('var x >= 42; minimize o: x;')
+    ampl_file.flush()
+    nl_filename = None
+    with util.temp_nl_file(ampl_file.name) as nl_file:
+      check_call([solver, nl_file.name, '-AMPL'], stdout=PIPE, stderr=PIPE)
+      sol_filename = os.path.splitext(nl_file.name)[0] + '.sol'
+      assert util.read_solution(ampl_file.name, sol_filename) == 42
 
 def test_mock_solver():
   try:
     sol_filename = None
     with tempfile.NamedTemporaryFile(suffix='.nl') as nl_file:
       sol_filename = os.path.splitext(nl_file.name)[0] + '.sol'
-      check_call([solver, nl_file.name])
+      check_call(['./mock-solver', nl_file.name])
     assert(os.path.exists(sol_filename))
   finally:
     os.remove(sol_filename)
@@ -45,7 +55,7 @@ def test_solve():
     ampl_file.flush()
     nl_filename = None
     sol_filename = None
-    with util.solve(ampl_file.name, solver=solver) as sf:
+    with util.solve(ampl_file.name, solver='./mock-solver') as (time, sf):
       nl_filename = os.path.splitext(sf)[0] + '.nl'
       sol_filename = sf
       assert(os.path.exists(nl_filename))
@@ -55,7 +65,7 @@ def test_solve():
     # Check if files are deleted even in case of KeyboardInterrupt.
     caught = False
     try:
-      with util.solve(ampl_file.name, solver=solver) as sf:
+      with util.solve(ampl_file.name, solver='./mock-solver') as (time, sf):
         nl_filename = os.path.splitext(sf)[0] + '.nl'
         sol_filename = sf
         assert(os.path.exists(nl_filename))
@@ -72,6 +82,6 @@ repo_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 def test_solve_timeout():
   start_time = time.time()
   with util.solve(os.path.join(repo_dir, 'nlmodels', 'camel1u.mod'),
-                  solver='couenne', timeout=1) as sf:
+                  solver=solver, timeout=1) as sf:
     elapsed_time = time.time() - start_time
     assert elapsed_time >= 1 and elapsed_time < 2
--- benchmark/util.py
+++ benchmark/util.py
@@ -1,8 +1,8 @@
 # Benchmark utilities
 
-import hashlib, os, tempfile, threading
+import hashlib, os, signal, tempfile, threading, time
 from contextlib import contextmanager
-from subprocess import check_call, Popen, PIPE
+from subprocess import check_call, Popen, PIPE, STDOUT
 
 def files(dirname, filenames):
   """
@@ -24,6 +24,17 @@ def files(dirname, filenames):
       results.append(os.path.join(dirname, filename))
   return results
 
+def sha1_file(filename):
+  "Computes a SHA-1 hash of file *filename*"
+  blocksize = 65536
+  hasher = hashlib.sha1()
+  with open(filename, 'rb') as f:
+    buf = f.read(blocksize)
+    while len(buf) > 0:
+      hasher.update(buf)
+      buf = f.read(blocksize)
+    return hasher.hexdigest()
+
 @contextmanager
 def temp_nl_file(ampl_filename):
   """
@@ -37,16 +48,23 @@ def temp_nl_file(ampl_filename):
     check_call(['ampl', '-ob' + os.path.splitext(f.name)[0], filename], cwd=dirname)
     yield f
 
-def sha1_file(filename):
-  "Computes a SHA-1 hash of file *filename*"
-  blocksize = 65536
-  hasher = hashlib.sha1()
-  with open(filename, 'rb') as f:
-    buf = f.read(blocksize)
-    while len(buf) > 0:
-      hasher.update(buf)
-      buf = f.read(blocksize)
-    return hasher.hexdigest()
+def read_solution(ampl_filename, sol_filename):
+  """
+  Read the solution of the model *ampl_filename* from *sol_filename*
+  and returns the objective value.
+  """
+  dirname, filename = os.path.split(ampl_filename)
+  p = Popen('ampl', cwd=dirname, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+  output = p.communicate('''
+    model "{}";
+    solution "{}";
+    display _obj[1];
+    '''.format(ampl_filename, sol_filename))[0]
+  obj = '_obj[1] = '
+  for line in output.split('\n'):
+    if line.startswith(obj):
+      return float(line[len(obj):])
+  return None
 
 @contextmanager
 def solve(ampl_filename, **kwargs):
@@ -61,16 +79,19 @@ def solve(ampl_filename, **kwargs):
     done = threading.Event()
     def kill_on_timeout():
       if not done.wait(timeout):
-        process.kill()
+        process.send_signal(signal.SIGINT)
     thread = threading.Thread(target=kill_on_timeout)
+    start_time = time.time()
     try:
-      process = Popen([kwargs.get('solver', 'minos'), nl_file.name, '-AMPL'], stdout=PIPE)
+      process = Popen([kwargs.get('solver', 'minos'), nl_file.name, '-AMPL'],
+                      stdout=PIPE, stderr=STDOUT)
       thread.start()
       process.communicate()
+      elapsed_time = time.time() - start_time
       # Stop the timeout thread.
       done.set()
       thread.join()
-      yield sol_filename
+      yield elapsed_time, sol_filename
     finally:
       # Remove the solution file if it exists.
       try:
