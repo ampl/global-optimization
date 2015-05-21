e60e1f8bb1fcf560e09652b1399334307a385c55
vitaut <victor.zverovich@gmail.com>
2015-05-21 12:09:07 -0700

    Add another benchmark

---
+++ util/cute-benchmark.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import glob, os
+from util import Benchmark, read_nl_header, repo_dir
+
+# Timeout in seconds
+TIMEOUT = 1
+
+LGO_LOCAL_SEARCH_MODE = 0
+LGO_MULTISTART_MODE   = 3
+
+models = sorted(glob.glob(os.path.join(repo_dir, 'cute', '*.mod')))
+
+with Benchmark(log='cute-lgo-local-search.yaml', solver='lgo', timeout=TIMEOUT,
+               solver_options={'opmode': LGO_LOCAL_SEARCH_MODE}) as b:
+  for model in models:
+    print(model)
+    b.run(model)
+
+exit(0)
+
+with Benchmark(log='cute-lgo-multistart.yaml', solver='lgo', timeout=TIMEOUT,
+                solver_options={'opmode': LGO_MULTISTART_MODE}, on_nl_file=update_options) as b:
+  for model in models:
+    print(model)
+    b.run(model)
