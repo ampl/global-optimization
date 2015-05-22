7d26a23b4100022f6b34b362153b3f1fbead8a1a
vitaut <victor.zverovich@gmail.com>
2015-05-22 10:30:01 -0700

    Enlarge cute benchmark

--- util/large-benchmark.py
+++ util/large-benchmark.py
@@ -10,10 +10,12 @@ TIMEOUT = 1
 LGO_LOCAL_SEARCH_MODE = 0
 LGO_MULTISTART_MODE   = 3
 
-models = sorted(glob.glob(os.path.join(repo_dir, 'cute', '*.mod')))
-models = [os.path.relpath(m, repo_dir) for m in models]
+models = []
+for subdir in ['cute', 'jdp', 'nlmodels']:
+  models += glob.glob(os.path.join(repo_dir, subdir, '*.mod')))
+models = sorted([os.path.relpath(m, repo_dir) for m in models])
 
-with Benchmark(log='cute-lgo-local-search.yaml', solver='lgo', timeout=TIMEOUT,
+with Benchmark(log='large-lgo-local-search.yaml', solver='lgo', timeout=TIMEOUT,
                solver_options={'opmode': LGO_LOCAL_SEARCH_MODE}) as b:
   for model in models:
     print(model)
@@ -21,7 +23,7 @@ with Benchmark(log='cute-lgo-local-search.yaml', solver='lgo', timeout=TIMEOUT,
 
 exit(0)
 
-with Benchmark(log='cute-lgo-multistart.yaml', solver='lgo', timeout=TIMEOUT,
+with Benchmark(log='large-lgo-multistart.yaml', solver='lgo', timeout=TIMEOUT,
                 solver_options={'opmode': LGO_MULTISTART_MODE}, on_nl_file=update_options) as b:
   for model in models:
     print(model)
