9550defdea2da559243b4efccacc5b9d96bc1034
vitaut <victor.zverovich@gmail.com>
2015-06-01 09:41:31 -0700

    Move merge_models to the util module

--- util/merge-problems.py
+++ util/merge-problems.py
@@ -1,73 +1,7 @@
 #!/usr/bin/env python
-# Merge multiple AMPL problems into a single one.
-# For example, two problems
-#   minimize o: f1(x);
-# and
-#   minimize o: f2(x);
-# are combined into a single problem
-#   minimize o: f1(x1) + f2(x2);
 
 from __future__ import print_function
-import ampl, glob, os, util
-
-def find_obj(nodes):
-  for i in range(len(nodes)):
-    node = nodes[i]
-    if isinstance(node, ampl.Decl) and (node.kind == 'minimize' or node.kind == 'maximize'):
-      return i
-
-class RenamingVisitor:
-  def __init__(self, names):
-    self.names = names
-
-  def visit_reference(self, expr):
-    expr.name = self.names.get(expr.name, expr.name)
-
-  def visit_subscript(self, expr):
-    self.visit_reference(expr)
-
-  def visit_paren(self, expr):
-    expr.arg.accept(self)
-
-  def visit_unary(self, expr):
-    expr.arg.accept(self)
-
-  def visit_binary(self, expr):
-    expr.lhs.accept(self)
-    expr.rhs.accept(self)
-
-  def visit_if(self, expr):
-    expr.condition.accept(self)
-    expr.true_expr.accept(self)
-    expr.false_expr.accept(self)
-
-  def visit_call(self, expr):
-    expr.arg.accept(self)
-
-  def visit_sum(self, expr):
-    expr.indexing.accept(self)
-    expr.arg.accept(self)
-
-  def visit_indexing(self, expr):
-    expr.set_expr.accept(self)
-
-def parse(model, suffix):
-  suffix = str(suffix)
-  with open(os.path.join(util.repo_dir, model), 'r') as f:
-    nodes = ampl.parse(f.read(), model)
-    # Rename declarations.
-    names = {}
-    visitor = RenamingVisitor(names)
-    for n in nodes:
-      if type(n) is ampl.Decl:
-        new_name = n.name + suffix
-        names[n.name] = new_name
-        n.name = new_name
-        if n.body:
-          n.body.accept(visitor)
-    # Find the first objective and partition the nodes around it.
-    obj_index = find_obj(nodes)
-    return nodes[:obj_index], nodes[obj_index], nodes[obj_index + 1:]
+import util
 
 def print_ast(nodes):
   for node in nodes:
@@ -77,11 +11,4 @@ models = util.get_models('casado', 'hansen')
 for m1 in models:
   for m2 in models:
     print(m1, m2)
-    head1, obj1, tail1 = parse(m1, 1)
-    head2, obj2, tail2 = parse(m2, 2)
-    obj = ampl.Decl('minimize', 'f')
-    # Invert sign if objectives are of different kinds.
-    obj.body = ampl.BinaryExpr('*', ampl.ParenExpr(obj1.body), ampl.ParenExpr(obj2.body))
-    if obj1.kind != obj2.kind:
-      obj.body = ampl.UnaryExpr('-', obj.body)
-    print_ast(head1 + head2 + [obj] + tail1 + tail2)
+    print_ast(util.merge_models(m1, m2))
--- util/util.py
+++ util/util.py
@@ -1,7 +1,7 @@
 # Benchmark utilities
 
 from __future__ import print_function
-import glob, hashlib, os, signal, tempfile, threading, time
+import ampl, glob, hashlib, os, signal, tempfile, threading, time
 from contextlib import contextmanager
 from datetime import datetime
 from subprocess import check_call, Popen, PIPE, STDOUT
@@ -290,3 +290,81 @@ class Benchmark:
     self.write_log_multiline('output', kwargs.get('output').replace('\b', ''))
     self.log.write('\n')
     self.log.flush()
+
+def find_obj(nodes):
+  for i in range(len(nodes)):
+    node = nodes[i]
+    if isinstance(node, ampl.Decl) and (node.kind == 'minimize' or node.kind == 'maximize'):
+      return i
+
+class RenamingVisitor:
+  def __init__(self, names):
+    self.names = names
+
+  def visit_reference(self, expr):
+    expr.name = self.names.get(expr.name, expr.name)
+
+  def visit_subscript(self, expr):
+    self.visit_reference(expr)
+
+  def visit_paren(self, expr):
+    expr.arg.accept(self)
+
+  def visit_unary(self, expr):
+    expr.arg.accept(self)
+
+  def visit_binary(self, expr):
+    expr.lhs.accept(self)
+    expr.rhs.accept(self)
+
+  def visit_if(self, expr):
+    expr.condition.accept(self)
+    expr.true_expr.accept(self)
+    expr.false_expr.accept(self)
+
+  def visit_call(self, expr):
+    expr.arg.accept(self)
+
+  def visit_sum(self, expr):
+    expr.indexing.accept(self)
+    expr.arg.accept(self)
+
+  def visit_indexing(self, expr):
+    expr.set_expr.accept(self)
+
+def parse(model, suffix):
+  suffix = str(suffix)
+  with open(os.path.join(repo_dir, model), 'r') as f:
+    nodes = ampl.parse(f.read(), model)
+    # Rename declarations.
+    names = {}
+    visitor = RenamingVisitor(names)
+    for n in nodes:
+      if type(n) is ampl.Decl:
+        new_name = n.name + suffix
+        names[n.name] = new_name
+        n.name = new_name
+        if n.body:
+          n.body.accept(visitor)
+    # Find the first objective and partition the nodes around it.
+    obj_index = find_obj(nodes)
+    return nodes[:obj_index], nodes[obj_index], nodes[obj_index + 1:]
+
+def merge_models(model1, model2):
+  """
+  Merge two AMPL models into a single one.
+  For example, two models
+    minimize o: f1(x);
+  and
+    minimize o: f2(x);
+  are combined into a single model
+    minimize o: f1(x1) * f2(x2);
+  """
+  head1, obj1, tail1 = parse(model1, 1)
+  head2, obj2, tail2 = parse(model2, 2)
+  obj = ampl.Decl('minimize', 'f')
+  # Invert sign if objectives are of different kinds.
+  obj.body = ampl.BinaryExpr('*', ampl.ParenExpr(obj1.body), ampl.ParenExpr(obj2.body))
+  if obj1.kind != obj2.kind:
+    obj.body = ampl.UnaryExpr('-', obj.body)
+  return head1 + head2 + [obj] + tail1 + tail2
