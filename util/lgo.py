4e49669c3b7c0bdbb56eb40ec05b5551d98d88f9
vitaut <victor.zverovich@gmail.com>
2015-06-01 12:32:53 -0700

    Move make_maxfct_setter to the lgo module

--- util/lgo.py
+++ util/lgo.py
@@ -1,5 +1,21 @@
-# LGO support module
+# LGO solver support
+
+import util
 
 # Possible values for the LGO opmode option.
 LOCAL_SEARCH_MODE = 0
 MULTISTART_MODE   = 3
+
+def make_maxfct_setter(k):
+  """
+  Return a function that sets g_maxfct, l_maxfct and maxnosuc LGO solver options
+  to k * 50 * (num_vars + num_cons + 2)**2.
+  """
+  def set(nl_file, args):
+    header = util.read_nl_header(nl_file.name)
+    maxfct = k * 50 * (header.num_vars + header.num_cons + 2)**2
+    solver_options = args['solver_options']
+    solver_options['g_maxfct'] = maxfct
+    solver_options['l_maxfct'] = maxfct
+    solver_options['maxnosuc'] = maxfct
+  return set
