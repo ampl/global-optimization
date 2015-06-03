1c881251fa974867e1fae2f1d04e14f917a315d8
vitaut <victor.zverovich@gmail.com>
2015-06-03 14:54:47 -0700

    Add a 3-dimensional benchmark

---
+++ util/montreal2015/3dim.py
@@ -0,0 +1,22 @@
+# Benchmark for CORS/INFORMS 2015
+# This benchmark is run on a collection of 3-dimensional problems generated
+# by combining problems from Casado and Hansen.
+
+import couenne, lgo
+from util import Config, get_problem_combinator, load_index
+
+inputs = get_problem_combinator(load_index('casado', 'hansen'), 3, 1000)
+
+# Timeout in seconds
+timeout = 60
+
+configs = [
+  Config('lgo', {'opmode': lgo.LOCAL_SEARCH_MODE}, suffix='local-search'),
+  Config('lgo', {'opmode': lgo.MULTISTART_MODE},
+         suffix='multistart', on_nl_file=lgo.make_maxfct_setter(2)),
+  Config('lgo', {'opmode': lgo.MULTISTART_MODE},
+         suffix='multistart-k4', on_nl_file=lgo.make_maxfct_setter(4)),
+  Config('minos'),
+  Config('baron'),
+  Config('couenne', couenne.options())
+]
