840db54b25f32be231cc74e8658f8315e1607d66
vitaut <victor.zverovich@gmail.com>
2015-06-23 14:42:09 -0700

    Add 5- and 10-dimensional benchmarks

---
+++ util/informs2015/10dim.py
@@ -0,0 +1,21 @@
+# This benchmark is run on a collection of 10-dimensional problems generated
+# by combining problems from Casado and Hansen.
+
+import couenne, lgo
+from util import Config, get_problem_combinator, load_index
+
+inputs = get_problem_combinator(load_index('casado', 'hansen'), 10, 1000)
+
+# Timeout in seconds
+timeout = 60 * 60
+
+configs = [
+  Config('lgo', {'opmode': lgo.LOCAL_SEARCH_MODE}, suffix='local-search'),
+  Config('lgo', {'opmode': lgo.MULTISTART_MODE},
+         suffix='multistart', on_nl_file=lgo.make_maxfct_setter(2)),
+  Config('lgo', {'opmode': lgo.MULTISTART_MODE},
+         suffix='multistart-k4', on_nl_file=lgo.make_maxfct_setter(4)),
+  Config('minos'),
+  Config('baron'),
+  Config('couenne', couenne.options())
+]
